92f1a02deb5fc4be8b3a5b82b7d47f200bc3b47f
Cesare Ferrari <cesare@maryandferrari.com>
2020-07-18 14:31:14 -0400

    Implement ex4

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,8 +1,19 @@
 def has_negatives(a):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    numbers = {}
+
+    # go through the list of numbers (a)
+    for num in a:
+        n = abs(num)
+
+        if n in numbers:
+            # if abs(number) is in dict: add 1 to value
+            numbers[n] += 1
+        else:
+            # otherwise, add it to the dict as a key with value 1
+            numbers[n] = 1
+
+    # return all keys with value 2
+    result = [k for k, v in numbers.items() if v == 2]
 
     return result
 
